929ae3469cf98429e2902907e1c777000bac92d3
KamranAshraf <umar17ash@gmail.com>
2020-07-03 19:16:46 +0530

    Handles all edge cases

--- indexRouter.js
+++ indexRouter.js
@@ -7,6 +7,7 @@ router.post('/', (req, res)=>{
     const H = req.body.height, e = req.body.e
     giveBounceAndGraphData(H, e)
     .then(data => {res.send(data)})
+    .catch(err => {res.send(err)})
 })
 
 router.get('/', (req, res)=>{
@@ -15,6 +16,7 @@ router.get('/', (req, res)=>{
 
 const giveBounceAndGraphData = (H, e) => {
     return new Promise((resolve, reject)=>{
+        
         let  T = Math.sqrt(2*H/9.81),          
         //no of bounces
         bounces = 0,                           
@@ -25,17 +27,33 @@ const giveBounceAndGraphData = (H, e) => {
         arr = [ HTdata(H, 0)] 
         arr = [...arr , ...heightTime(0, 0, T, "-", H)]
         arr.push( HTdata(0, T) )
-        while(h > 0.5){
-            bounces++ 
-            h = h*e**2
-            temp1 = temp2 + T*e**bounces
-            arr = [...arr , ...heightTime(-9.81*(temp1-temp2) ,temp2, temp1, "+", h)]
-            arr.push( HTdata(h, temp1))
-            tempi = temp1
-            temp1 = temp1 + T*e**bounces
-            arr = [...arr , ...heightTime(0, tempi, temp1, "-", h)]
-            arr.push( HTdata(0, temp1) )  
-            temp2 = temp1
+        
+        // On invalid inputs
+        if(e<0 || e>1 || H<0 ){
+            reject("invalid inputs")
+        }
+
+        //coefficient of restitution = 1
+        if(e === 1){
+            arr = [...arr , ...heightTime(-9.81*T, T, 2*T, "+", H)]
+            arr.push( HTdata(H, 2*T))
+            bounces = "infinite"
+        }
+
+        // coefficient of restitution > 0 and < 1
+        if(e>0 && e<1){                            
+            while(h > 0.5){
+                bounces++ 
+                h = h*e**2
+                temp1 = temp2 + T*e**bounces
+                arr = [...arr , ...heightTime(-9.81*(temp1-temp2) ,temp2, temp1, "+", h)]
+                arr.push( HTdata(h, temp1))
+                tempi = temp1
+                temp1 = temp1 + T*e**bounces
+                arr = [...arr , ...heightTime(0, tempi, temp1, "-", h)]
+                arr.push( HTdata(0, temp1) )  
+                temp2 = temp1
+            }
         }
         fs.writeFileSync( __dirname + '/data.json', `{"bounces" : ${bounces}, "height_time" : ${JSON.stringify(arr)}}`, (err)=>{console.log(err)})
         resolve( {bounces, arr} )
@@ -44,6 +62,7 @@ const giveBounceAndGraphData = (H, e) => {
 
 const HTdata = (H, T) => ({"height":H, "time":T})
 
+//Calculates height and time in between the extremes
 const heightTime= (initialVelocity, initialTime, finalTime, sign, H)=>{
         arr = []
         let temp1Time = initialTime + 1, tempTime = 1
@@ -56,10 +75,6 @@ const heightTime= (initialVelocity, initialTime, finalTime, sign, H)=>{
             tempTime++
     }
     return arr
-}
-
-const velocity = (u, t)=>{
-    return  u+(9.81*t)
 }
 
 module.exports = router
