0cc87bf465b2d8e64f53602f5b26658129654dbd
KamranAshraf <umar17ash@gmail.com>
2020-07-03 18:57:03 +0530

    gives correct cordinates at a time interval of 1sec

--- indexRouter.js
+++ indexRouter.js
@@ -16,21 +16,24 @@ router.get('/', (req, res)=>{
 const giveBounceAndGraphData = (H, e) => {
     return new Promise((resolve, reject)=>{
         let  T = Math.sqrt(2*H/9.81),          
-        bounces = 0,                           //no of bounces
+        //no of bounces
+        bounces = 0,                           
         temp1 = 0 , temp2 = T, tempi,  h = H,
-        u = 0 ,v = finalVelocity(u, T, "-")   //final and initial velocity in a single up/down motion
-        arr = [ HTdata(H, 0)] //height and time
+        //final and initial velocity in a single up/down motion
+        u = 0 ,v = u+(9.81*T)  
+        //height and time
+        arr = [ HTdata(H, 0)] 
         arr = [...arr , ...heightTime(0, 0, T, "-", H)]
         arr.push( HTdata(0, T) )
         while(h > 0.5){
             bounces++ 
             h = h*e**2
             temp1 = temp2 + T*e**bounces
-            
+            arr = [...arr , ...heightTime(-9.81*(temp1-temp2) ,temp2, temp1, "+", h)]
             arr.push( HTdata(h, temp1))
             tempi = temp1
             temp1 = temp1 + T*e**bounces
-            
+            arr = [...arr , ...heightTime(0, tempi, temp1, "-", h)]
             arr.push( HTdata(0, temp1) )  
             temp2 = temp1
         }
@@ -44,9 +47,9 @@ const HTdata = (H, T) => ({"height":H, "time":T})
 const heightTime= (initialVelocity, initialTime, finalTime, sign, H)=>{
         arr = []
         let temp1Time = initialTime + 1, tempTime = 1
-        while(tempTime < finalTime){
+        while(temp1Time < finalTime){
             if(sign === "+") 
-                arr.push(HTdata((initialVelocity*tempTime - (.5*9.81*tempTime**2)) ,temp1Time))
+                arr.push(HTdata(-(initialVelocity*tempTime + (.5*9.81*tempTime**2)) ,temp1Time))
             else
                 arr.push(HTdata(H-(initialVelocity*tempTime + (.5*9.81*tempTime**2)) ,temp1Time))
             temp1Time++
@@ -55,8 +58,8 @@ const heightTime= (initialVelocity, initialTime, finalTime, sign, H)=>{
     return arr
 }
 
-const finalVelocity = (u, t, sign)=>{
-    return sign==="+" ? u-(9.81*t) : u+(9.81*t)
+const velocity = (u, t)=>{
+    return  u+(9.81*t)
 }
 
 module.exports = router
